86d91b28a9f014db709e4ffe33b80cbc92af7f3e
Mansea <6210742034@student.tu.ac.th>
2022-04-01 19:16:03 +0700

    Update Users.js

--- routes/Users.js
+++ routes/Users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('../models/Users');
 
 router.get('/:id', async (req, res) => {
-  var users = await User.find({_id: req.params.id}, {_id: 0,date: 0,__v: 0});
+  var users = await User.find({_id: req.params.id}, {date: 0,__v: 0});
   res.send({success: true ,user: users});
 });
 
