3616db1e4081c30fc6be4c87dbd9fdd4a0c9c4f3
Mansea <6210742034@student.tu.ac.th>
2022-04-12 14:00:45 +0700

    Update Favorite.js

--- routes/Favorite.js
+++ routes/Favorite.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
+const Shop = require('../models/Shops')
 
 router.post("/product", async (req, res) => {
-  const user = await User.findOne({ _id: req.body._id });
+  const user = await User.findOne({ _id: req.body._id } , {date: 0, __v: 0});
   if (user) {
     await user.product_lists.push(req.body.product_id);
     user.save();
@@ -67,26 +68,32 @@ router.post("/shop", async (req, res) => {
     }
   });
 
-  router.get("/shop", async (req, res) => {
-    const user = await User.findOne({ _id: req.body._id });
-    if (user) {
-      let lists = await user.shop_lists;
-      res.send(lists);
-    } else {
-      res.status(400).send({
-        success: false,
-        message: "User Id is invalid!!",
-      });
-    }
+  // router.get("/shop", async (req, res) => {
+  //   const user = await User.findOne({ _id: req.body._id });
+  //   if (user) {
+  //     let lists = await user.shop_lists;
+  //     res.send(lists);
+  //   } else {
+  //     res.status(400).send({
+  //       success: false,
+  //       message: "User Id is invalid!!",
+  //     });
+  //   }
+  // });
+
+  router.post("/getShop", async (req, res) => {
+    const shops = await Shop.find({ 'ord': {$in: req.body.ord} });
+    res.send(shops)
+  
   });
   
-  router.delete("/shop", async (req, res) => {
+  router.put("/shop", async (req, res) => {
     const user = await User.findOne({ _id: req.body._id });
     if (user) {
       let lists = await user.shop_lists;
       var check = lists.indexOf(req.body.shop_id);
       if (check != -1) {
-        lists.splice(lists.indexOf(req.body.shop_id), 1);
+        lists.splice(lists.indexOf(req.body.shop_id), 1);      
         user.save();
         res.send(user);
       } else {
